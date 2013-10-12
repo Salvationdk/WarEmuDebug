329529d33b539a26b0590888ef95b3a95c53e9dd
zozooo <zozooo@gmail.com>
2013-10-12 06:09:37 -0500

    first commit

---
+++ WarServer/GameServer/Listener.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Reflection;
+namespace WarhammerEmu.GameServer
+{
+    public class Listener
+    {
+        private string hostIp;
+        private int hostPort;
+        protected static Socket m_socket;
+
+        public Listener(string hostIp, int hostPort)
+        {
+            this.hostIp = hostIp;
+            this.hostPort = hostPort;
+        }
+        public void Run()
+        {
+            Log.Debug("GameServer: Listening for connections..");
+            try
+            {
+                LoadCryptHandler();
+                m_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                m_socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontLinger, true);
+                m_socket.Bind(new IPEndPoint(IPAddress.Parse(hostIp), hostPort));
+                m_socket.Listen(10);
+                m_socket.BeginAccept(new AsyncCallback(OnConnect), null);
+            }
+            catch (SocketException se)
+            {
+                Log.Warn("GameServer: Listener exception.." + se);
+            }
+        }
+        private void OnConnect(IAsyncResult asyn)
+        {
+
+            var socket = m_socket.EndAccept(asyn);
+            Connection connection = new Connection(socket,this);
+            m_socket.BeginAccept(new AsyncCallback(OnConnect), null);
+        }
+
+        public readonly Dictionary<string, ICryptHandler> m_cryptHandlers = new Dictionary<string, ICryptHandler>();
+        public void LoadCryptHandler()
+        {
+           // Log.Info("TCPManager", "Loading Crypt Handler");
+
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (Type type in assembly.GetTypes())
+                {
+                    // Pick up a class
+                    if (type.IsClass != true)
+                        continue;
+
+                    CryptAttribute[] crypthandler =
+                        (CryptAttribute[])type.GetCustomAttributes(typeof(CryptAttribute), true);
+
+                    if (crypthandler.Length > 0)
+                    {
+                     //   Log.Debug("TCPManager", "Registering crypt " + crypthandler[0]._CryptName);
+                        m_cryptHandlers.Add(crypthandler[0]._CryptName, (ICryptHandler)Activator.CreateInstance(type));
+                    }
+                }
+            }
+        }
+
+        public ICryptHandler GetCrypt(string name)
+        {
+            if (m_cryptHandlers.ContainsKey(name))
+                return m_cryptHandlers[name];
+            else
+                return null;
+        }
+
+
+        static public int GetTimeStamp()
+        {
+            return (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
+        }
+    }
+}
---
+++ WarServer/Log.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace WarhammerEmu
+{
+    public static class Log
+    {
+
+        private static readonly ConsoleColor warn = ConsoleColor.DarkRed;
+        private static readonly ConsoleColor trace = ConsoleColor.Yellow;
+        private static readonly ConsoleColor debug = ConsoleColor.DarkGreen;
+
+
+        private static void write(object obj, ConsoleColor color)
+        {
+            Console.ResetColor();
+            Console.ForegroundColor = color;
+            Console.WriteLine(obj);
+        }
+        public static void Debug(object obj)
+        {
+
+            string sformat = string.Format("[Debug]: {0}", obj);
+            write(sformat, debug);
+        }
+        public static void Trace(object obj)
+        {
+            string sformat = string.Format("[Trace]: {0}", obj);
+            write(sformat, trace);
+        }
+        public static void Warn(object obj)
+        {
+            string sformat = string.Format("[Warn]: {0}", obj);
+            write(sformat, warn);
+        }
+        public static void Error(object obj)
+        {
+            string sformat = string.Format("[Error]: {0}", obj);
+            write(sformat, warn);
+        }
+        public static void Error(string t, object obj)
+        {
+            string sformat = string.Format("[Error]: {0} {1}", t, obj);
+            write(sformat, warn);
+        }
+    }
+}
